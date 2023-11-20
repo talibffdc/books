1afc5c762be211cb6d8e28aaee5549ffc09e4504
talib.cpp <talibffdc@gmail.com>
2023-11-20 15:47:03 +0530

    working update button

--- src/SavedData.js
+++ src/SavedData.js
@@ -3,6 +3,15 @@ import React, { useState, useEffect } from 'react';
 export default function SavedData() {
   const [books, setBooks] = useState([]);
 
+  const[tempTitle, setTempTitle]=useState();
+  const [tempAuthor, setTempAuthor]=useState();
+  const [tempSerial, setTempSerial]= useState();
+  const [tempEmail, setTempEmail]= useState();
+  const [tempPrice, setTempPrice]=useState();
+
+
+
+
   useEffect(() => {
     // Fetch data from local storage on component mount
     const data = localStorage.getItem('books');
@@ -20,6 +29,40 @@ export default function SavedData() {
     // Update local storage with the updated array
     localStorage.setItem('books', JSON.stringify(updatedBooks));
   };
+ 
+
+  //  ye sab index cannot find naa ho isliye likha gya hain bs
+  const [tempIndex, setTempIndex] = useState("");
+
+
+    const handleUpdate=(index)=>{
+
+      setTempIndex(index);
+
+      setTempTitle(books[index].title);
+      setTempAuthor(books[index].author);
+      setTempSerial(books[index].serial);
+      setTempEmail(books[index].email);
+      setTempPrice(books[index].price);
+
+    }
+
+    const handleSave=(index)=>{
+      var tempBooks=[...books];
+      tempBooks[index].title=tempTitle;
+      tempBooks[index].author=tempAuthor;
+      tempBooks[index].serial=tempSerial;
+      tempBooks[index].email=tempEmail;
+      tempBooks[index].price=tempPrice;
+
+      setBooks(tempBooks);
+      localStorage.setItem("books", JSON.stringify(tempBooks));
+        }
+ 
+
+
+
+  
 
  
   return (
@@ -52,7 +95,7 @@ export default function SavedData() {
             <td>{book.price}</td>
             <td>
 
-            <button className="btn btn-danger mx-2" data-bs-toggle="modal" data-bs-target="#exampleModal"> Update  </button>
+            <button className="btn btn-danger mx-2" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={()=>handleUpdate(index)}> Update  </button>
 
             <button
           className="btn btn-danger" onClick={() => handleDelete(index)} >
@@ -79,17 +122,18 @@ export default function SavedData() {
         </div>
         <div className="modal-body">
 
-        <input className="form-control my-2" placeholder="Change Book Name" type="text"/>
-        <input className="form-control my-2" placeholder="Change Author" type="text"/>
-        <input className="form-control my-2" placeholder="Change Serial" type="text"/>
-        <input className="form-control my-2" placeholder="Change Email" type="text"/>
-        <input className="form-control my-2" placeholder="Change Price" type="text"/>
+        <input className="form-control my-2" placeholder="Change Book Name" value={tempTitle} onChange={(e)=>setTempTitle(e.target.value)} type="text"/>
+
+        <input className="form-control my-2" placeholder="Change Author" value={tempAuthor} onChange={(e)=> setTempAuthor(e.target.value)}type="text"/>
+        <input className="form-control my-2" placeholder="Change Serial" value={tempSerial} onChange={(e)=> setTempSerial(e.target.value)} type="text"/>
+        <input className="form-control my-2" placeholder="Change Email" value={tempEmail} onChange={(e)=> setTempEmail(e.target.value)}type="text"/>
+        <input className="form-control my-2" placeholder="Change Price" value={tempPrice} onChange={(e)=> setTempPrice(e.target.value)}type="text"/>
 
 
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+        <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={()=> handleSave(tempIndex)} >Save changes</button>
       </div>
     </div>
   </div>
